4fd0b8718c6e95d23e784aa4f8e0f2de838a824b
Shreya Satyakam <89099439+Shreyasatyakam@users.noreply.github.com>
2022-02-26 00:30:17 +0530

    Q3. shreya

---
+++ Task 2/Question 3.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+void main() {
+ 
+  Map<String, Object> birthdays = {
+    "Narendra Modi": "17/09/1950",
+    "Mother Teresa": "26/08/1910",
+    "A.P.J Abdul Kalam": "15/10/1931",
+  };
+
+  print("\nWe know the birthdays of the following people: \n");
+
+  birthdays.forEach((key, value) {
+    print(key);
+  });
+
+  stdout.write("\nWho's birthday do you want to know? ");
+  String choice = stdin.readLineSync();
+
+  print("\n$choice's birthday is ${birthdays[choice]}\n");
+}
