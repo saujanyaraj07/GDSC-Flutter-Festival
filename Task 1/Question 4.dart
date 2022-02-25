b73abad0c3a13661e45786342372dfbcc8d3541a
aditi944 <aditijha201200@gmail.com>
2022-02-25 11:46:09 +0530

    by aditi

---
+++ Task 1/Question 4.dart
@@ -0,0 +1,20 @@
+void main() {
+  
+   sum(47, 57);
+   difference(47, 57);
+   division(47, 57);
+   multiplication(47, 57);
+ }
+ 
+
+void sum(int num1, int num2) =>
+  print('Sum is ${(num1+num2)}');
+
+void difference(int num1, int num2) =>
+  print('Differnce is ${(num1-num2)}');
+
+void division(int num1, int num2) =>
+  print('Division is ${(num1/num2)}');
+
+void multiplication(int num1, int num2) =>
+  print('Multiplication is ${(num1*num2)}');
