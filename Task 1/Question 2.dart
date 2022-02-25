b5a9e9cdb6f8f3fed77b6f2654ac8eb4c4e22dce
aditi944 <aditijha201200@gmail.com>
2022-02-25 11:10:21 +0530

    by adti

---
+++ Task 1/Question 2.dart
@@ -0,0 +1,15 @@
+void main() { 
+  int num=567;
+ 
+  print("The number is $num");
+   check(num);
+}  
+ check(number) { 
+  if(number%2==0){
+    print("The number is even");
+  }
+  else{
+    print("The number is odd");
+  }
+    
+}   
