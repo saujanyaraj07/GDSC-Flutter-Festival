1ec0912e866d79a7cfe1610351a0be94974401d4
aditi944 <aditijha201200@gmail.com>
2022-02-25 10:56:40 +0530

    by aditi

---
+++ Task 1/Question 1.dart
@@ -0,0 +1,13 @@
+void main()
+{
+    int number = 3556;
+  
+    // Condition is true
+    if (number%5==0) { 
+      // This will be printed
+        print("The number is divisible by 5"); 
+    }
+  else{
+    print("The number is not idvisible by 5");
+  }
+}
