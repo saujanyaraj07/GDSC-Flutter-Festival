c2a229d32e12936575665be922ba0b8fcbc66900
Saujanya Raj <88873588+saujanyaraj07@users.noreply.github.com>
2022-02-26 22:19:25 +0530

    Update Question 2.dart

--- Task 2/Question 2.dart
+++ Task 2/Question 2.dart
@@ -1,17 +1,17 @@
-void main() {
-  List<int> a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
-
-  int i = 0;
-  List<int> l = [];
-
-  for (var e in a) {
-    if (++i % 2 == 0) {
-      l.add(e);
+void main() 
+{
+  List a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+  print("Original list: ");
+  print(a);
+  final b = <int>[];
+  int c=0;
+  for( var i = 0 ; i < a.length ; i++ )
+  {
+    if(a[i]%2==0)
+    {
+      b.add(a[i]);
     }
   }
-  print(l);
-  print([
-    for (var e in a)
-      if (++i % 2 == 0) e
-  ]);
-}
+  print("New list with Even Numbers: ");
+  print(b);
+} 
